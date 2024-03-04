bce0ae8e0fa7204e1c09e38fb2890244e6621388
Shivam Mishra <mshivam718@gmail.com>
2024-03-04 19:15:22 +0530

    Add files via upload

---
+++ mainTest.py
@@ -0,0 +1,20 @@
+import cv2
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+
+model = load_model('BrainTumor10EpochsCategorical.h5')
+
+image = cv2.imread('C:\\Users\\mshiv\\Documents\\Capstone 2\\pred\\pred5.jpg')
+
+img = Image.fromarray(image)
+
+img = img.resize((64,64))
+
+img = np.array(img)
+
+input_img = np.expand_dims(img,axis=0)
+
+result = model.predict_classes(input_img)
+print(result)
+
